1a66f70792cb911fb383343f45ec4847f35d3a03
Harsha Konda <hkonda@andrew.cmu.edu>
2017-12-01 14:49:09 -0500

    Check cycles

--- src/topologicalsort/CourseScheduleii.java
+++ src/topologicalsort/CourseScheduleii.java
@@ -3,11 +3,11 @@ package topologicalsort;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class CourseSchedule {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
+public class CourseScheduleii {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        if (numCourses == 0) return new int[0];
 
-        if (numCourses == 0) return false;
-
+        int[] course = new int[numCourses];
         HashMap<Integer, ArrayList<Integer>> edgeList = new HashMap<>();
 
         for (int i = 0; i < prerequisites.length; i++) {
@@ -26,13 +26,20 @@ public class CourseSchedule {
         boolean[] visiting = new boolean[numCourses];
         boolean possible = true;
 
+        ArrayList<Integer> list = new ArrayList<>();
         for (int i = 0; i < visited.length; i++)
-            possible = possible && (dfs(visited, visiting, edgeList, i));
+            possible = possible && (dfs(visited, visiting, list, edgeList, i));
 
-        return possible;
+        if (!possible)
+            return new int[0];
+
+        for (int i = 0; i < list.size(); i++)
+            course[i] = list.get(i);
+
+        return course;
     }
 
-    public boolean dfs(boolean[] visited, boolean[] visiting, HashMap<Integer, ArrayList<Integer>> edgeList, int index) {
+    public boolean dfs(boolean[] visited, boolean[] visiting, ArrayList<Integer> list, HashMap<Integer, ArrayList<Integer>> edgeList, int index) {
         if (visited[index])
             return true;
 
@@ -45,7 +52,9 @@ public class CourseSchedule {
         boolean value = true;
         if (edgeList.containsKey(index))
             for (Integer n : edgeList.get(index))
-                value = value && dfs(visited, visiting, edgeList, n);
+                value = value && dfs(visited, visiting, list, edgeList, n);
+
+        list.add(index);
 
         visiting[index] = false;
         visited[index] = true;
